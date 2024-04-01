61ea85e55c0760c9c124591cb0391908b84e2100
nanjini <nanjini6@naver.com>
2024-04-01 17:20:10 +0900

    24.04.01 수정 - CommonApiApplication.java   @ContextConfiguration(classes = {SpringConfig.class}) 제거 - SpringConfig.java   DataSourceTransactionManager 추가 - UserServiceImpl.java   @Transactional 추가

--- sboot-backend-common/com-sboot-backend-common-api/src/main/java/CommonApiApplication.java
+++ sboot-backend-common/com-sboot-backend-common-api/src/main/java/CommonApiApplication.java
@@ -1,18 +1,16 @@
-import com.sboot.backend.common.SpringConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootApplication
-@ContextConfiguration(classes = {SpringConfig.class})
 @ComponentScan("com.sboot.backend.common")
 @MapperScan("com.sboot.backend.common")
 public class CommonApiApplication {
     private static final Logger logger = LogManager.getLogger(CommonApiApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CommonApiApplication.class, args);
         logger.info("Application Start!");
